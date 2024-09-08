8fc7c539e5ef855395d12b478650d561d31273a7
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-08 19:53:17 +0530

    fix: prevent reload using shortcut

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,32 @@
-import { appWindow } from '@tauri-apps/api/window';
+import { appWindow } from "@tauri-apps/api/window";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/base.css";
 
+document.getElementById("titlebar-minimize")?.addEventListener("click", () => appWindow.minimize());
+document.getElementById("titlebar-maximize")?.addEventListener("click", () => appWindow.toggleMaximize());
+document.getElementById("titlebar-close")?.addEventListener("click", () => appWindow.close());
 
-document.getElementById('titlebar-minimize')?.addEventListener('click', () => appWindow.minimize());
-document.getElementById('titlebar-maximize')?.addEventListener('click', () => appWindow.toggleMaximize());
-document.getElementById('titlebar-close')?.addEventListener('click', () => appWindow.close());
+window.addEventListener("keydown", (ev) => {
+  if (ev.ctrlKey && ev.key === "r") {
+    ev.preventDefault();
+  }
+
+  if (ev.ctrlKey && ev.shiftKey && ev.key === "I") {
+    console.log("DONT OPEN");
+    ev.preventDefault();
+  }
+});
+
+window.addEventListener("keyup", (ev) => {
+  if (ev.ctrlKey && ev.shiftKey && ev.key === "I") {
+    console.log("DONT OPEN");
+    ev.preventDefault();
+  }
+});
+
+document.querySelector(".titlebar")?.addEventListener("contextmenu", (ev) => {
+  ev.preventDefault();
+});
 
 createApp(App).mount("#app");
