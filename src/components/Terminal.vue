618e3cc5f9ef3c6eea124c18933a99f76f19fac3
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-09-08 18:54:48 +0530

    feat: integrate xterm.js

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
 import { Terminal } from '@xterm/xterm';
+import "@xterm/xterm/css/xterm.css";
+
 import { onMounted } from 'vue';
 
-
 onMounted(() => {
-  var terminal = new Terminal();
+  var terminal = new Terminal({
+    cursorBlink: 'block',
+    fontFamily: 'monospace'
+  });
   terminal.open(document.getElementById('terminal-container')!);
+  terminal.write("Welcome to ZTerm\n\r\n\r");
   terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
   console.log("hello world");
 
   terminal.onData((data) => {
     console.log('Terminal input:', data);
-    // backend
   });
 });
 
@@ -35,20 +39,5 @@ onMounted(() => {
   background-color: black;
   color: white;
 }
-
-/* 
-textarea {
-  border: none;
-  width: 100%;
-  background-color: black;
-  color: white; 
-  outline: none;
-  resize: none;
-  font-size: 1.5rem;
-}
-
-textarea::-webkit-scrollbar {
-  display: none;
-} */
 
 </style>
