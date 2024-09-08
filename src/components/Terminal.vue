3a18278e62f824dfe25ed34af17d79b50d5a5188
Billy <billydevbusiness@gmail.com>
2024-09-08 17:01:24 +0500

    fix: Fixed the conspiracy

---
+++ src/components/Terminal.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const command = ref("");
+
+// async function greet() {
+//   greetMsg.value = await invoke("greet", { name: name.value });
+// }
+</script>
+
+<template>
+  <textarea id="stream" v-model="command" />
+</template>
+
+
+<style scoped>
+
+textarea {
+  border: none;
+  width: 100%;
+  background-color: black;
+  color: white;
+  outline: none;
+  resize: none;
+}
+
+textarea::-webkit-scrollbar {
+  display: none;
+}
+
+</style>
